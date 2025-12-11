5af803ab1ed1b63a570c1b056fa9245787e85a0c
Buddhika Rathnayake <rathnayakearmb@gmail.com>
2025-12-11 13:21:59 +0000

    created method for in order traversal

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -50,4 +50,12 @@ public class BinarySearchTree{
             }
 
         }
+
+        public void inOrder(Node root){
+            if(root != null){
+                inOrder(root.left);
+                System.out.print(root.data + " ");
+                inOrder(root.right);
+            }
+        }
 }
