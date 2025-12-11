dfb3aa41f34adf7fbc3f42da582c5066c3c8ab1e
Buddhika Rathnayake <rathnayakearmb@gmail.com>
2025-12-11 13:20:16 +0000

    fix: correct search method name and return statement in BinarySearchTree

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -7,3 +7,47 @@ class Node{
         left = right = null;
     }
 }
+
+public class BinarySearchTree{
+    Node root;
+
+    BinarySearchTree(){
+        root =  null;
+    }
+        public void insert(int value){
+            root = insertRec(root,value);
+        }
+
+        public void insertRec(Node root,int value){
+            if(root == null){
+                root = new Node(value);
+                return root;
+            }
+
+            if(value < root.data){
+                root.left = insertRec(root.left,value);
+            } else if(value > root.data){
+                root.right = insertRec(root.right,value);
+            }return root;
+        }
+
+        public boolean search(int value){
+            return searchRec(root,value);
+        }
+        public boolean serachRec(Node root,int value){
+            if(root == null){
+                return false;
+            }
+
+            if(root.data == value){
+                return true;
+            }
+
+            if(value < root.data){
+                return searchRec(root.left,value);
+            }else{
+                reurn searchRec(root.right,value);
+            }
+
+        }
+}
